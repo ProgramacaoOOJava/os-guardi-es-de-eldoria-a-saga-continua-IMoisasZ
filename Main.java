8beb29b1f1fcef14f450e13aff04b31da74476a1
Moisés Barbosa dos Santos <68626845+IMoisasZ@users.noreply.github.com>
2026-08-20 02:21:52 +0000

    Criado a classe main, onde é possivel fazer o cadastro dos herois e mostrar suas infromações, confrome solicitado para o nivel novato.

--- Main.java
+++ Main.java
@@ -1,24 +1,35 @@
-package eldoria;
+package Tema2.novato.desafio;
 
+// Imports para utilização de List e ArrayList
 import java.util.ArrayList;
+import java.util.List;
 
-/**
- * Classe principal que demonstra os conceitos de POO aplicados no sistema de personagens de Eldoria.
- * Demonstra: herança, polimorfismo, encapsulamento, instanceof, equals() e hashCode().
- */
+// Classe main
 public class Main {
-    
+    // Metodo main (principal)
     public static void main(String[] args) {
-        System.out.println("=== BEM-VINDOS AO REINO DE ELDORIA ===");
-        System.out.println("Os Guardiões se preparam para a batalha!\n");
-        
-        // Criando uma lista polimórfica de personagens
-        
-        // Adicionando Magos à lista
-        
-        // Adicionando Guerreiros à lista
-        
-        // Percorrendo a lista e demonstrando polimorfismo
-        
-}
+        // Informações...
+        System.out.println("\n=== BEM VINDOS AO REINO DE ELDORIA ===");
+        System.out.println("Os Guardiões se preparam para a batalha\n");
 
+        // Criando os personagens/herois
+        List<Personagem> herois = new ArrayList<>();
+        herois.add(new Guerreiro("Thor", "Guerreiro", 10, 10, 254.85,
+                "Usa seu matelo Myonir para lançar raios poderosos!"));
+        herois.add(new Mago("Magaiver", "Mago", 4, 6, 80.25,
+                "Utiliza de qualquer tipo de coisa para criar algo para lhe tirar de enrascadas!"));
+        herois.add(new Guerreiro("Kratos", "Guerreiro", 10, 10, 1454.74,
+                "Pode utilizar varias armas e ainda tem o poder da caixa de pandora que o deixa com a capacidade de matar deuses!"));
+        herois.add(new Mago("Harry", "Mago", 9, 7, 118.15,
+                "Pode usar sua varinha para lançar poderes magicos e conjurar feitiços poderosos!"));
+
+        // Mostrando os herois cadastrados
+        System.out.print("\n=== CADASTRO DOS HEROIS ===");
+        for (Personagem p : herois) {
+            p.exibirStatus();
+
+            p.usarHabilidade();
+            System.out.println();
+        }
+    }
+}
